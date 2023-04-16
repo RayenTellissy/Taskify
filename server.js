f72f508f09f9dec50d29ee141c795d0e660d8294
RayenTellissy <rayentellissy571@gmail.com>
2023-04-16 01:19:14 +0100

    .

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
-const http = require('http');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/todos') {
-    // code to retrieve all the todos from the fake API
-    const todos = [
-      { id: 1, title: 'Todo 1', completed: false },
-      { id: 2, title: 'Todo 2', completed: true },
-      { id: 3, title: 'Todo 3', completed: false },
-    ];
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(todos));
-  } else {
-    res.end('Hello, World!');
-  }
-});
+app.get("/", (req, res) => res.type('html').send(html));
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`app listening on port ${port}!`));
