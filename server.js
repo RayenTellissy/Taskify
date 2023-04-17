9c1d7c0a0f49ff922dc43e6467cd672c405da94c
RayenTellissy <rayentellissy571@gmail.com>
2023-04-17 14:11:42 +0100

    .

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
-
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-
 app.post('/', (req, res) => {
   res.send('Received a POST request');
 });
-
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
